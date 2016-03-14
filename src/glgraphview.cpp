ea5a6de22153382204912a8b4a08e0ec7b5c337b
Gergely Nyiri <gergely.nyiri@gmail.com>
2016-03-14 10:12:48 +0100

    Update OpenGL code

--- src/glgraphview.cpp
+++ src/glgraphview.cpp
@@ -63,7 +63,12 @@ static const char* fragmentShaderSource =
 //--------------------------------------------------
 glgraphview::glgraphview(QWidget* p_parent) :
   QOpenGLWidget(p_parent),
-  m_program(0)
+  m_program(0),
+  m_core(false),
+  m_projMatrixLoc(0),
+  m_mvMatrixLoc(0),
+  m_normalMatrixLoc(0),
+  m_lightPosLoc(0)
 {
   m_core = QCoreApplication::arguments().contains(QStringLiteral("--coreprofile"));
 }
@@ -76,6 +81,7 @@ glgraphview::~glgraphview()
 void glgraphview::cleanup()
 {
   makeCurrent();
+  m_obuf.destroy();
   delete m_program;
   m_program = 0;
   doneCurrent();
@@ -99,20 +105,46 @@ void glgraphview::initializeGL()
   glClearColor(0, 0, 0, 1);
 
   m_program = new QOpenGLShaderProgram;
+  // set vertex shader code
   m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, m_core ? vertexShaderSourceCore : vertexShaderSource);
+  // set fragment shader code
   m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, m_core ? fragmentShaderSourceCore : fragmentShaderSource);
+  // vertex is on the 0th location
   m_program->bindAttributeLocation("vertex", 0);
+  // normal is on the 1st location
   m_program->bindAttributeLocation("normal", 1);
+  // link shaders
   m_program->link();
+  // make this shader current
   m_program->bind();
-
+  // get location of projMatrix in the shader
   m_projMatrixLoc = m_program->uniformLocation("projMatrix");
+  // get location of mvMatrix in the shader
   m_mvMatrixLoc = m_program->uniformLocation("mvMatrix");
+  // get location of normalMatrix in the shader
   m_normalMatrixLoc = m_program->uniformLocation("normalMatrix");
+  // get location of lightPos in the shader
   m_lightPosLoc = m_program->uniformLocation("lightPos");
 
-  m_vao.create();
-  QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
+  m_vertices.push_back(0.1);
+  m_vertices.push_back(0.0);
+  m_vertices.push_back(0.5);
+
+  m_vertices.push_back(0.0);
+  m_vertices.push_back(0.0);
+  m_vertices.push_back(1.0);
+
+  m_vertices.push_back(1.0);
+  m_vertices.push_back(0.5);
+  m_vertices.push_back(0.5);
+
+  m_vertices.push_back(0.0);
+  m_vertices.push_back(0.0);
+  m_vertices.push_back(1.0);
+
+  m_obuf.create();
+  m_obuf.bind();
+  m_obuf.allocate(m_vertices.data(), m_vertices.size() * sizeof(GLfloat));
 
   setupVertexAttribs();
 
@@ -120,19 +152,18 @@ void glgraphview::initializeGL()
   m_camera.translate(0, 0, -1);
 
   m_program->setUniformValue(m_lightPosLoc, QVector3D(0, 0, 70));
-
   m_program->release();
 }
 //--------------------------------------------------
 void glgraphview::setupVertexAttribs()
 {
-  m_logoVbo.bind();
+  m_obuf.bind();
   QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
   f->glEnableVertexAttribArray(0);
   f->glEnableVertexAttribArray(1);
   f->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), 0);
   f->glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<void *>(3 * sizeof(GLfloat)));
-  m_logoVbo.release();
+  m_obuf.release();
 }
 //--------------------------------------------------
 void glgraphview::paintGL()
@@ -143,12 +174,13 @@ void glgraphview::paintGL()
 
   m_world.setToIdentity();
 
-  QOpenGLVertexArrayObject::Binder vaoBinder(&m_vao);
   m_program->bind();
   m_program->setUniformValue(m_projMatrixLoc, m_proj);
   m_program->setUniformValue(m_mvMatrixLoc, m_camera * m_world);
   QMatrix3x3 normalMatrix = m_world.normalMatrix();
   m_program->setUniformValue(m_normalMatrixLoc, normalMatrix);
+
+  glDrawArrays(GL_LINES, 0, m_vertices.size() / 6);
 
   m_program->release();
 }
--- src/glgraphview.h
+++ src/glgraphview.h
@@ -7,6 +7,8 @@
 #include <QOpenGLBuffer>
 #include <QMatrix4x4>
 
+#include <vector>
+
 QT_FORWARD_DECLARE_CLASS(QOpenGLShaderProgram)
 
 /*!
@@ -26,30 +28,87 @@ public:
     * \brief DTOR
     */
   ~glgraphview();
+  /*!
+   * \brief
+   */
   QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+  /*!
+   * \brief
+   */
   QSize sizeHint() const Q_DECL_OVERRIDE;
+  /*!
+   * \brief
+   */
   void cleanup();
-
 protected:
+  /*!
+   * \brief
+   */
   void initializeGL() Q_DECL_OVERRIDE;
+  /*!
+   * \brief
+   */
   void paintGL() Q_DECL_OVERRIDE;
+  /*!
+   * \brief
+   */
   void resizeGL(int width, int height) Q_DECL_OVERRIDE;
 
 private:
+  /*!
+   * \brief
+   */
   void setupVertexAttribs();
-  bool m_core;
-  int m_projMatrixLoc;
-  int m_mvMatrixLoc;
-  int m_normalMatrixLoc;
-  int m_lightPosLoc;
-
-  QOpenGLVertexArrayObject m_vao;
+  /*!
+   * \brief
+   * the shader program
+   */
   QOpenGLShaderProgram* m_program;
-  QOpenGLBuffer m_logoVbo;
-
+  /*!
+   * \brief
+   */
+  bool m_core;
+  /*!
+   * \brief
+   */
+  int m_projMatrixLoc;
+  /*!
+   * \brief
+   */
+  int m_mvMatrixLoc;
+  /*!
+   * \brief
+   */
+  int m_normalMatrixLoc;
+  /*!
+   * \brief
+   */
+  int m_lightPosLoc;
+  /*!
+   * \brief
+   */
+  QOpenGLVertexArrayObject m_vao;
+  /*!
+   * \brief m_obuf
+   * GPU dynamic memory handle
+   */
+  QOpenGLBuffer m_obuf;
+  /*!
+   * \brief
+   */
   QMatrix4x4 m_proj;
+  /*!
+   * \brief
+   */
   QMatrix4x4 m_camera;
+  /*!
+   * \brief
+   */
   QMatrix4x4 m_world;
+  /*!
+   * \brief m_vertices
+   */
+  std::vector<GLfloat> m_vertices;
 };
 
 #endif // GLGRAPHVIEW_H
